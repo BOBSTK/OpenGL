bde1a3c6a5fe4ced8048c72f2787d0f9c8582d7c
BOBSTK <729078846@qq.com>
2020-11-11 19:56:05 +0800

    修改

--- Code/OpenGL/OpenGL/main.cpp
+++ Code/OpenGL/OpenGL/main.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
+using namespace std;
+#include<glad\glad.h>
 #include <GLFW\glfw3.h>
-int main() {
 
+
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	return 0;
 }
